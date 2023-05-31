e836918e081dd825f6aa576b87418c1ae0ab42b1
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-05-31 20:22:12 +0100

    update entity

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ http://locahost:8000/currency-exchange/from/USD/to/INR
 
 Currency Conversion Service
 http://locahost:8000/currency-conversion/from/USD/to/INR/quantity/10
+
+
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO CURRENCY_EXCHANGE (id, currency_from, currency_to, conversion_multiple) values (1000, 'USD', 'INR', 80);
+INSERT INTO CURRENCY_EXCHANGE (id, currency_from, currency_to, conversion_multiple) values (1001, 'EUR', 'INR', 85);
+INSERT INTO CURRENCY_EXCHANGE (id, currency_from, currency_to, conversion_multiple) values (1002, 'GBP', 'INR', 100);
