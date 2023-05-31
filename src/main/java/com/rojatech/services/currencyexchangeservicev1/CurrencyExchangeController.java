82e9a2c23d7d62929265f5ea887fd777ef2fdadf
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-05-31 19:45:56 +0100

    add environment properties

--- src/main/java/com/rojatech/services/currencyexchangeservicev1/CurrencyExchangeController.java
+++ src/main/java/com/rojatech/services/currencyexchangeservicev1/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.rojatech.services.currencyexchangeservicev1;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(80));
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(80));
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		return currencyExchange;
 	}
 }
