e21f08acce32267033963873aa08f727e8a163fa
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-05-31 18:46:41 +0100

    create currency exchange service

---
+++ src/main/java/com/rojatech/services/currencyexchangeservicev1/CurrencyExchangeServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.rojatech.services.currencyexchangeservicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rojatech/services/currencyexchangeservicev1/CurrencyExchangeServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojatech.services.currencyexchangeservicev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
