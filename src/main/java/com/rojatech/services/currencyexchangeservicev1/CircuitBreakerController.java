d0165186de921130bdade5737bc3414a7845bea9
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-06-03 11:40:52 +0100

    add bulkhead

--- src/main/java/com/rojatech/services/currencyexchangeservicev1/CircuitBreakerController.java
+++ src/main/java/com/rojatech/services/currencyexchangeservicev1/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -19,7 +20,8 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	//@Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
 	//@CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
-	@RateLimiter(name = "default")
+	//@RateLimiter(name = "default")
+	@Bulkhead(name = "sample-api")
 	public String sampleApi() {
 		logger.info("sample-api call received");
 //		ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/dummypath", String.class);
