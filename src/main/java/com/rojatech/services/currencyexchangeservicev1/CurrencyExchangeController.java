a8af4d9538f5bd8ac2c550ef02643d67b7acb319
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-06-04 19:35:33 +0100

    add logger

--- src/main/java/com/rojatech/services/currencyexchangeservicev1/CurrencyExchangeController.java
+++ src/main/java/com/rojatech/services/currencyexchangeservicev1/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.rojatech.services.currencyexchangeservicev1;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+			
 	@Autowired
 	private CurrencyExchangeRepository repository;
 	
@@ -19,6 +23,8 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		logger.info("retriveExchangeValue called with {} to {}", from, to);
+		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if (currencyExchange == null) {
 			throw new RuntimeException("Unable to find data for " + from + " to " + to);
